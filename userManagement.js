21ba8ef3a7361c2f9f634f37361eba58d8c28af4
shri124 <spate615@uic.edu>
2023-08-26 14:36:02 -0500

    deleted print statements

--- userManagement.js
+++ userManagement.js
@@ -1,10 +1,8 @@
 function findUser(username, botObject) {
-    console.log('function 1')
     return botObject.botItems.find(userBot => userBot.name === username)
 }
 
 function addUser(username, botObject) {
-    console.log('function 2')
     let userBot = {
         name: username,
         joke: [],
